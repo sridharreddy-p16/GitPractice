0b95cce43375c9d165aa0b2e626d4477be85a623
srredhar p <sridharreddy.p16@gmail.com>
2023-03-07 19:28:07 +0530

    ElectricEngine class added

---
+++ ElectricEngine.java
@@ -0,0 +1,8 @@
+public class ElectricEngine{
+	 public static void start() {
+		 System.out.println("ElectricEngine engine Started")
+	 }
+	 public static void stop() {
+		 System.out.println("ElectricEngine engine Stopped")
+	 }
+}
