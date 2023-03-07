61e872899b8d82f4bed409d8619a461808606cde
srredhar p <sridharreddy.p16@gmail.com>
2023-03-07 20:01:50 +0530

    PetrolEngine class stop method added

--- PetrolEngine.java
+++ PetrolEngine.java
@@ -2,5 +2,7 @@ public class PetrolEngine{
 	 public static void start() {
 		 System.out.println("Petrol engine Started")
 	 }
-	
+	 public static void stop() {
+		 System.out.println("Petrol engine Stopped")
+	 }
 }
