b9349a911799179d3ec626eb3a54f30b7c1a6137
srredhar p <sridharreddy.p16@gmail.com>
2023-03-07 19:32:21 +0530

    ElectricEngine running method added

--- ElectricEngine.java
+++ ElectricEngine.java
@@ -2,6 +2,11 @@ public class ElectricEngine{
 	 public static void start() {
 		 System.out.println("ElectricEngine engine Started")
 	 }
+	 
+	 public static void running() {
+		 System.out.println("ElectricEngine engine running")
+	 }
+	 
 	 public static void stop() {
 		 System.out.println("ElectricEngine engine Stopped")
 	 }
